9b0c5cb89750ddc89ddfc16ed22fc29bb98d4aaf
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-07-27 15:04:30 +0530

    files added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,30 @@
 import CustomiseButtons from "./CustomiseButtons";
+import Linkdin from "../assets/Negative.svg";
+import Instagram from "../assets/Original.svg";
+
 function Body() {
   return (
     <>
-      <div className=" flex justify-around items-center gap-7 mt-24  ">
+      <div className=" md:flex sm:flex-wrap md:justify-around md:items-center md:gap-7 mt-24  ">
         <div>
-          <div className="text-[#000000] font-['Montserrat', 'sans-serif'] font-extrabold uppercase text-9xl tracking-tight">
+          <div className="text-[#000000] font-['Montserrat', 'sans-serif'] font-extrabold uppercase md:text-9xl tracking-tight">
             <h3>abhiraj</h3>
             <h3>shilkar</h3>
           </div>
-          <CustomiseButtons link={"https://openai.com/index/chatgpt/"} buttonname="visit linkdin" className=""></CustomiseButtons>
+          <div className="mt-10 flex justify-self-start items-center gap-5 ">
+            <CustomiseButtons
+              buttonimage={Linkdin}
+              link={"https://openai.com/index/chatgpt/"}
+              buttonname="visit linkdin"
+              className=""
+            ></CustomiseButtons>
+            <CustomiseButtons
+              buttonimage={Instagram}
+              link={"https://openai.com/index/chatgpt/"}
+              buttonname="visit instagram"
+              className=""
+            ></CustomiseButtons>
+          </div>
         </div>
         <div>
           <p>
--- src/components/CustomiseButtons.jsx
+++ src/components/CustomiseButtons.jsx
@@ -1,9 +1,9 @@
 const CustomiseButtons=({buttonname,link,buttonimage})=>{
     return (
         <>
-        <a href={link} className="text-mono uppercase">
-            <button>
-                <img src={buttonimage} alt="" />{buttonname}
+        <a href={link} >
+            <button className="flex justify-center items-center gap-2 capitalize font-mono pt-[1.9px] pb-[1.9px] pr-4 pl-4 rounded-3xl border-[0.7px]">
+                <img className="w-[18px]" src={buttonimage} alt="" />{buttonname}
                 </button>
                 </a>
         </>
