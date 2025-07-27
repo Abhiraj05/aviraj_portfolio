24ccddebd9e680668606f1f2b8d34593fd7a7b3c
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-07-27 12:42:05 +0530

    files added

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+function Body() {
+  return (
+    <>
+      <div className="text-white flex justify-center align-middle gap-7 capitalize text-8xl text-shadow-amber-300">
+        <div>hello</div>
+        <div>world</div>
+      </div>
+    </>
+  );
+}
+
+export default Body;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function NavBar(){
+    return(
+        <>
+      <nav className="flex justify-around align-middle gap-8">
+        <div>
+           <img src="" alt="aviraj" />
+        </div>
+        <div>
+            <ul className="flex justify-center align-middle gap-3 text-white">
+                <li>home</li>
+                <li>about me</li>
+                <li>resume</li>
+            </ul>
+        </div>
+      </nav>
+        </>
+    )
+}
+export default NavBar;
